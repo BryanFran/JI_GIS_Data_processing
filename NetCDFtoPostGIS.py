74942568dba09d1715519a254dfc818749995452
Bryan Ramírez <42655811+BryanFran@users.noreply.github.com>
2024-10-21 13:08:16 -0500

    Add files via upload

---
+++ NetCDFtoPostGIS.py
@@ -0,0 +1,186 @@
+import xarray as xr
+import geopandas as gpd
+from sqlalchemy import create_engine, text, inspect
+from geoalchemy2 import Geometry
+import os
+from tqdm import tqdm
+import pandas as pd
+from shapely.geometry import Point, LineString
+import pypsa
+import numpy as np
+
+def clean_database(engine):
+    print("Starting database cleanup...")
+    inspector = inspect(engine)
+    
+    protected_tables = ['spatial_ref_sys', 'geography_columns', 'geometry_columns', 'raster_columns', 'raster_overviews']
+    
+    with engine.connect() as connection:
+        tables = inspector.get_table_names(schema='public')
+        
+        for table in tables:
+            if table not in protected_tables:
+                print(f"Deleting table: {table}")
+                connection.execute(text(f"DROP TABLE IF EXISTS {table} CASCADE"))
+        
+        connection.commit()
+    
+    print("Database cleaned, maintaining essential PostGIS tables.")
+
+
+def create_postgis_table(engine, table_name, columns):
+    if table_name.lower() in ["all", "user", "group"]:
+        table_name = f"table_{table_name}"
+    
+    print(f"Creating table {table_name} in the database...")
+    column_defs = []
+    for col, dtype in columns.items():
+        if col == 'country_code':
+            continue 
+        col = col.replace(':', '_').replace('.', '_').replace(' ', '_')
+        if dtype == 'geometry':
+            column_defs.append(f"{col} GEOMETRY")
+        elif isinstance(dtype, np.dtype):
+            if np.issubdtype(dtype, np.floating):
+                column_defs.append(f"{col} FLOAT")
+            elif np.issubdtype(dtype, np.integer):
+                column_defs.append(f"{col} INTEGER")
+            elif np.issubdtype(dtype, np.bool_):
+                column_defs.append(f"{col} BOOLEAN")
+            else:
+                column_defs.append(f"{col} TEXT")
+        elif str(dtype).startswith('float'):
+            column_defs.append(f"{col} FLOAT")
+        elif str(dtype).startswith('int'):
+            column_defs.append(f"{col} INTEGER")
+        elif str(dtype) == 'bool':
+            column_defs.append(f"{col} BOOLEAN")
+        else:
+            column_defs.append(f"{col} TEXT")
+    
+    columns_sql = ", ".join(column_defs)
+    sql = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        id SERIAL PRIMARY KEY, 
+        country_code CHAR(2),
+        {columns_sql}
+    )
+    """
+    
+    with engine.connect() as conn:
+        conn.execute(text(sql))
+    print(f"Table {table_name} created successfully.")
+
+def load_data_to_postgis(n, engine, country_code):
+    tables = ['buses', 'carriers', 'generators', 'lines', 'loads', 'storage_units', 'stores']
+    
+    for table_name in tables:
+        print(f"Loading data from {table_name} for country {country_code} into the database...")
+        
+        df = getattr(n, table_name)
+        
+        index_name = df.index.name if df.index.name else table_name.capitalize()
+        df = df.reset_index()
+        df = df.rename(columns={'index': index_name})
+        
+        columns = [index_name] + [col for col in df.columns if col != index_name]
+        df = df[columns]
+        
+        dtypes = df.dtypes.to_dict()
+        
+        if table_name == 'buses':
+            ac_carrier = ["AC"]
+            ac_buses = n.buses.query("carrier in @ac_carrier").index
+            df = df[df[index_name].isin(ac_buses)]
+        
+        df['country_code'] = country_code
+        
+        if table_name in ['buses', 'lines']:
+            if table_name == 'buses':
+                df['geometry'] = df.apply(lambda row: Point(row['x'], row['y']) if 'x' in row and 'y' in row else None, axis=1)
+            elif table_name == 'lines':
+                print("Procesando tabla 'lines'")
+                print(f"Columnas en df: {df.columns}")
+                print(f"Primeras filas de df:\n{df.head()}")
+                
+                buses = n.buses[['x', 'y']]
+                df = df.merge(buses.add_suffix('_0'), left_on='bus0', right_index=True)
+                df = df.merge(buses.add_suffix('_1'), left_on='bus1', right_index=True)
+                
+                df['geometry'] = df.apply(lambda row: 
+                    LineString([(row['x_0'], row['y_0']), (row['x_1'], row['y_1'])])
+                    if all(x in row and pd.notnull(row[x]) for x in ['x_0', 'y_0', 'x_1', 'y_1']) 
+                    else None, axis=1)
+                
+                print(f"Geometrías creadas: {df['geometry'].notna().sum()}")
+        
+        df.columns = [col.replace(':', '_').replace('.', '_').replace(' ', '_') for col in df.columns]
+        
+        if 'geometry' in df.columns:
+            gdf = gpd.GeoDataFrame(df, geometry='geometry', crs="EPSG:4326")
+        else:
+            gdf = df
+        
+        columns = {**dtypes, 'country_code': 'object', 'geometry': 'geometry'}
+        create_postgis_table(engine, table_name, columns)
+        
+        print(f"Inserting data into table {table_name}...")
+        try:
+            if 'geometry' in gdf.columns:
+                gdf_with_geometry = gdf[gdf['geometry'].notnull()]
+                gdf_without_geometry = gdf[gdf['geometry'].isnull()]
+                if not gdf_with_geometry.empty:
+                    gdf_with_geometry.to_postgis(table_name, engine, if_exists='append', index=False)
+                if not gdf_without_geometry.empty:
+                    gdf_without_geometry.to_sql(table_name, engine, if_exists='append', index=False)
+            else:
+                gdf.to_sql(table_name, engine, if_exists='append', index=False)
+            
+            print(f"Data inserted into {table_name}:")
+            print(gdf.head())
+            print(f"Data shape: {gdf.shape}")
+        except Exception as e:
+            print(f"Error inserting data into table {table_name}: {e}")
+            print("First 5 rows of data:")
+            print(gdf.head())
+            print("\nData type information:")
+            print(gdf.dtypes)
+        print(f"Data from {table_name} for country {country_code} inserted successfully.")
+        
+def process_netcdf(file_path, engine):
+    print(f"Processing NetCDF file: {file_path}")
+    country_code = os.path.basename(file_path).split('_')[0]
+    n = pypsa.Network(file_path)
+    load_data_to_postgis(n, engine, country_code)
+
+def main():
+    db_params = {
+        'dbname': 'pypsa_earth_db',
+        'user': 'postgres',
+        'password': 'oetpostgres',
+        'host': '34.68.214.20',
+        'port': '5432'
+    }
+    engine = create_engine(f"postgresql://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['dbname']}")
+    
+    print("Starting database cleanup...")
+    clean_database(engine)
+
+    netcdf_files = [
+        r"D:\Work\OET\Dashboard\Yerbol code\NetCDF\AU_2021.nc",
+        r"D:\Work\OET\Dashboard\Yerbol code\NetCDF\BR_2021.nc",
+        r"D:\Work\OET\Dashboard\Yerbol code\NetCDF\CO_2021.nc",
+        r"D:\Work\OET\Dashboard\Yerbol code\NetCDF\DE_2021.nc",
+        r"D:\Work\OET\Dashboard\Yerbol code\NetCDF\IN_2021.nc",
+        r"D:\Work\OET\Dashboard\Yerbol code\NetCDF\IT_2021.nc",
+        r"D:\Work\OET\Dashboard\Yerbol code\NetCDF\MX_2021.nc",
+        r"D:\Work\OET\Dashboard\Yerbol code\NetCDF\NG_2021.nc",
+        r"D:\Work\OET\Dashboard\Yerbol code\NetCDF\US_2021.nc",
+        r"D:\Work\OET\Dashboard\Yerbol code\NetCDF\ZA_2021.nc"
+    ]
+
+    for netcdf_file in tqdm(netcdf_files, desc="Processing NetCDF files"):
+        process_netcdf(netcdf_file, engine)
+
+if __name__ == "__main__":
+    main()
